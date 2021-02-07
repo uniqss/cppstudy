60e00f96173498c5e0bfb6d74c5f514d90fc1fdf
uniqss <uniqs@163.com>
2021-02-07 09:35:42 +0800

    i try daily

--- cppcalljs2/bin/readfile.js
+++ cppcalljs2/bin/readfile.js
@@ -3,6 +3,10 @@ print("readfile.js begin");
 print("aaa");
 
 
+// 获取CPP的返回
+version = version();
+print(version);
+
 
 
 print("readfile.js end");
---
+++ cppcalljs2/bin/shelltest.js
@@ -0,0 +1,13 @@
+print("helloworld.js begin");
+
+//r = read("readfile.js");
+
+load("readfile.js");
+
+ret = testAdd(123, 321, 1234);
+
+print(ret);
+
+print("param1", "param2");
+
+print("helloworld.js end");
