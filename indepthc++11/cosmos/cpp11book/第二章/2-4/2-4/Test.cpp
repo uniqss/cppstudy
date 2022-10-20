d7479e4ebbd705ff33744b6f301ee9e032de443d
uniqs <uniqs@163.com>
2022-10-20 23:47:02 +0800

    ddu

--- indepthc++11/cosmos/cpp11book/第二章/2-4/2-4/Test.cpp
+++ indepthc++11/cosmos/cpp11book/第二章/2-4/2-4/Test.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
+using std::cout;
+using std::endl;
 
-void PrintT(int& t)
-{
-	std::cout << "lvalue" << std::endl;
+void PrintT(int& t) {
+    ++t;
+    std::cout << "### lvalue t:" << t << std::endl;
 }
 
 template <typename T>
-void PrintT(T&& t)
-{
-	std::cout << "rvalue" << std::endl;
+void PrintT(T&& t) {
+    ++t;
+    std::cout << "*** rvalue t:" << t << std::endl;
 }
 
 template <typename T>
-void TestForward(T&& v)
-{
-	PrintT(v);
-	PrintT(std::forward<T>(v));
-	PrintT(std::move(v));
+void TestForward(T&& v) {
+    PrintT(v);
+    PrintT(std::forward<T>(v));
+    PrintT(std::move(v));
+    cout << v << endl;
 }
 
-void Test()
-{
-	TestForward(1);
-	int x = 1;
-	TestForward(x);
-	TestForward(std::forward<int>(x));
+void Test() {
+    TestForward(1);
+    // const是不行的， int& 和 std::forward<int> 都不行
+    // const int x = 1;
+    int x = 1;
+    TestForward(x);
+    cout << x << endl;
+    TestForward(std::forward<int>(x));
+    cout << x << endl;
 }
 
-int main(void)
-{
-	Test();
+int main(void) {
+    Test();
 
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
