8a8693ef1e552aaceace7b1dd1b95ace17577ffc
uniqs <uniqs@163.com>
2022-11-14 08:18:52 +0000

    ggs

--- boost_study/talking_async/1/step_1.cpp
+++ boost_study/talking_async/1/step_1.cpp
@@ -1,16 +1,14 @@
 #include <array>
 #include <iostream>
-#include <memory>
 #include <asio.hpp>
-#include "dbg.h"
+#include <memory>
 
 using asio::awaitable;
 using asio::buffer;
 using asio::co_spawn;
-using asio::co_spawn;
+using asio::detached;
 using asio::ip::tcp;
 using asio::use_awaitable;
-using asio::detached;
 
 struct proxy_state {
     proxy_state(tcp::socket client) : client(std::move(client)) {}
@@ -21,12 +19,10 @@ struct proxy_state {
 using proxy_state_ptr = std::shared_ptr<proxy_state>;
 
 awaitable<void> client_to_server(proxy_state_ptr state) {
-    PPID;
     try {
         std::array<char, 1024> data;
         for (;;) {
             auto n = co_await state->client.async_read_some(buffer(data), use_awaitable);
-            PPID;
             co_await async_write(state->server, buffer(data, n), use_awaitable);
         }
     } catch (const std::exception& e) {
@@ -34,14 +30,11 @@ awaitable<void> client_to_server(proxy_state_ptr state) {
         state->server.close();
     }
 }
-
 awaitable<void> server_to_client(proxy_state_ptr state) {
-    PPID;
     try {
         std::array<char, 1024> data;
         for (;;) {
             auto n = co_await state->server.async_read_some(buffer(data), use_awaitable);
-            PPID;
             co_await async_write(state->client, buffer(data, n), use_awaitable);
         }
     } catch (const std::exception& e) {
@@ -51,65 +44,40 @@ awaitable<void> server_to_client(proxy_state_ptr state) {
 }
 
 awaitable<void> proxy(tcp::socket client, tcp::endpoint target) {
-    PPID;
     auto state = std::make_shared<proxy_state>(std::move(client));
 
     co_await state->server.async_connect(target, use_awaitable);
 
     auto ex = state->client.get_executor();
     co_spawn(ex, client_to_server(state), detached);
-
     co_await server_to_client(state);
 }
 
-awaitable<void> listen(tcp::acceptor& acceptor, tcp::endpoint target) {
-    PPID;
+awaitable<void> listen(asio::ip::tcp::acceptor& acceptor, asio::ip::tcp::endpoint target) {
     for (;;) {
         auto client = co_await acceptor.async_accept(use_awaitable);
 
-        PPID;
         auto ex = client.get_executor();
         co_spawn(ex, proxy(std::move(client), target), detached);
     }
 }
-
-awaitable<void> awaitable_test(const char* uid) {
-    PT << uid << "2222222" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    PT << uid << "33333" << std::endl;
-    co_return;
-}
-
-int main(int argc, const char* argv[]) {
-    PPID;
+int main(int argc, const char** argv) {
     try {
         if (argc != 5) {
-            std::cerr << "Usage: proxy";
-            std::cerr << " <listen_address> <listen_port>";
-            std::cerr << " <target_address> <target_port>\n";
+            std::cerr << "Usage:" << argv[0] << " <host_addr> <host_port> <target_addr> <target_port> \n";
             return 1;
         }
-
         asio::io_context ctx;
-
-#if 0
-        PT << "111111" << std::endl;
-        co_spawn(ctx, awaitable_test("aaa"), asio::detached);
-        co_spawn(ctx, awaitable_test("bbb"), asio::detached);
-        co_spawn(ctx, awaitable_test("ccc"), asio::detached);
-        PT << "444444" << std::endl;
-#endif
-
-        auto listen_endpoint = *tcp::resolver(ctx).resolve(argv[1], argv[2], tcp::resolver::passive);
-
-        auto target_endpoint = *tcp::resolver(ctx).resolve(argv[3], argv[4]);
-
-        tcp::acceptor acceptor(ctx, listen_endpoint);
+        asio::ip::tcp::endpoint host_endpoint =
+            *asio::ip::tcp::resolver(ctx).resolve(argv[1], argv[2], asio::ip::tcp::resolver::passive);
+        asio::ip::tcp::endpoint target_endpoint = *asio::ip::tcp::resolver(ctx).resolve(argv[3], argv[4]);
+        asio::ip::tcp::acceptor acceptor(ctx, host_endpoint);
 
         co_spawn(ctx, listen(acceptor, target_endpoint), detached);
 
         ctx.run();
     } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
+        std::cerr << "Exception:" << e.what() << std::endl;
     }
+    return 0;
 }
