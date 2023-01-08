df9d4a7311851eb3f822efecb7e70cefeb4bce89
wanwei <uniqs@163.com>
2023-01-08 17:48:48 +0800

    1

--- indepthc++11/cosmos/cpp11book/第3章/3-2/3-2/Test.cpp
+++ indepthc++11/cosmos/cpp11book/第3章/3-2/3-2/Test.cpp
@@ -13,13 +13,15 @@ struct fn_class {
 };
 
 int main(void) {
-#if 1
+    // c++17及以后，这样写
+#if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || __cplusplus >= 201703L)
     // 
     typedef std::invoke_result<decltype(fn), int>::type A;  // int
     typedef std::invoke_result<fn_ref, int>::type B;        // int
     typedef std::invoke_result<fn_ptr, int>::type C;        // int
     typedef std::invoke_result<fn_class, int>::type D;      // int
 #else
+    // c++14及以前，这样写
     typedef std::result_of<decltype(fn)&(int)>::type A;  // int
     typedef std::result_of<fn_ref(int)>::type B;         // int
     typedef std::result_of<fn_ptr(int)>::type C;         // int
@@ -39,8 +41,7 @@ int main(void) {
         // std::cout << "A: " << std::is_same<int, std::invoke_result<fn(123)>>::value << std::endl;
         std::cout << "A: " << std::is_same<int, A>::value << std::endl;
     }
-    cout << "这题没搞懂，以c++11/"
-            "14标准编译是ok的，但是升到17和20以后这个程序的正确写法没找到，等学完17和20的三本书再研究"
+    cout << "result_of(17及以后要用invoke_result而且用法有细微的差别)的用法，注意result_of能在不调用函数的情况下，拿到它的返回类型"
          << endl;
 
     system("pause");
