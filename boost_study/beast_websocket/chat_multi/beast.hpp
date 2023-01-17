310936c292db10e0875b7079d5bb2df174c36b8e
uniqss <uniqs@163.com>
2023-01-17 02:07:08 +0000

    r

--- boost_study/beast_websocket/chat_multi/beast.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <boost/beast.hpp>
-
-namespace beast = boost::beast;          // from <boost/beast.hpp>
-namespace http = beast::http;            // from <boost/beast/http.hpp>
-namespace websocket = beast::websocket;  // from <boost/beast/websocket.hpp>
